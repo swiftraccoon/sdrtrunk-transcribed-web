c9d258133e7574cee03ab8978ef8c4b751b67f3c
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-10-27 23:08:40 -0400

    ah

--- email.js
+++ email.js
@@ -1,18 +1,18 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.protonmail.ch',
+  host: 'smtp.email.gov',
   port: 587,
   secure: false,
   auth: {
-    user: 'dylan@spindale.host', // replace with your ProtonMail username
-    pass: 'WUAWC5ZXFD8AAYFH'  // replace with your ProtonMail password
+    user: 'user', // replace with your ProtonMail username
+    pass: 'pass'  // replace with your ProtonMail password
   }
 });
 
 const sendEmail = async (to, subject, text) => {
   const mailOptions = {
-    from: 'dylan@spindale.host', // sender address
+    from: 'user', // sender address
     to: to,          // list of receivers
     subject: subject,// subject line
     text: text       // plain text body
