bd2e8e112a2d0283988a6dea31057a4b740fb923
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-10-29 02:11:39 -0400

    Add tests for checking transcriptions and sending emails
    This commit adds new tests for checking transcriptions of files created since server boot and sending emails if a regex match is found. It also includes tests to ensure that files are not checked more than once and that the files are checked in descending order. Additionally, it ensures that the check process stops once a file older than the server boot time is reached. The tests use stubs to mock file system operations and email sending.

---
+++ test/checkTranscriptions.test.js
@@ -0,0 +1,67 @@
+const chai = require('chai');
+const assert = chai.assert;
+const sinon = require('sinon');
+const checkTranscriptions = require('../checkTranscriptions');
+const fs = require('fs').promises;
+const sendEmail = require('../email');
+
+describe('checkTranscriptions', () => {
+  let readDirRecursiveStub, readFileStub, sendEmailStub;
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    readDirRecursiveStub = sandbox.stub(fs, 'readdir');
+    readFileStub = sandbox.stub(fs, 'readFile');
+    sendEmailStub = sandbox.stub(sendEmail);
+  });
+  
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should check files created since server boot', async () => {
+    readDirRecursiveStub.resolves(['file1', 'file2']);
+    readFileStub.resolves(JSON.stringify({ text: 'some text' }));
+    
+    await checkTranscriptions();
+    
+    assert.isTrue(readDirRecursiveStub.calledOnce);
+  });
+
+  it('should send an email if regex matches', async () => {
+    readDirRecursiveStub.resolves(['file1']);
+    readFileStub.resolves(JSON.stringify({ text: 'match this text' }));
+    
+    await checkTranscriptions();
+    
+    assert.isTrue(sendEmailStub.calledOnce);
+  });
+
+  it('should not check any files more than once', async () => {
+    readDirRecursiveStub.resolves(['file1', 'file1']);
+    readFileStub.resolves(JSON.stringify({ text: 'some text' }));
+    
+    await checkTranscriptions();
+    await checkTranscriptions();
+    
+    assert.isTrue(readFileStub.calledOnce);
+  });
+
+  it('should start with the newest file and go down in descending order', async () => {
+    readDirRecursiveStub.resolves(['file2', 'file1']);
+    readFileStub.resolves(JSON.stringify({ text: 'some text' }));
+    
+    await checkTranscriptions();
+    
+    assert.isTrue(readDirRecursiveStub.returnValues[0][0] === 'file2');
+  });
+
+  it('should stop once a file older than server boot time is reached', async () => {
+    readDirRecursiveStub.resolves(['new_file', 'old_file']);
+    readFileStub.resolves(JSON.stringify({ text: 'some text' }));
+    
+    await checkTranscriptions();
+    
+    assert.isTrue(readFileStub.calledOnce);
+  });
+});
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const Mocha = require('mocha');
 const mocha = new Mocha();
 
 mocha.addFile('./search.test.js');
+mocha.addFile('./checkTranscriptions.test.js');
 // mocha.addFile('./server.test.js');
 
 mocha.run((failures) => {
