6cb2ab70278360bd49aa34d3bf40240436c0020d
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-10-28 20:10:50 -0400

    test

--- test/search.test.js
+++ test/search.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const search = require('../search');
-
+const config = require('../example.config');
 
 
 describe('searchTranscriptions', () => {
