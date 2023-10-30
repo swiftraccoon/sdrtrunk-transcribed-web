cd9756ff340fb9c58478eddd97184f6ce2ee9e59
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-10-30 02:03:02 -0400

    Refactor sending email in checkTranscriptions.js
    Instead of sending an email with just the transcription text, now it sends the file name, all transcriptions combined, and a search URL.

--- checkTranscriptions.js
+++ checkTranscriptions.js
@@ -72,7 +72,7 @@ const processFile = async (filePath, fileName) => {
         console.log(`regex: ${regex}`)
         const allText = Object.values(transcription).join(', ');
         if (regex.test(allText)) {
-            await sendEmailWithRateLimit(sub.email, `${sub.regex}`, `${fileName} \n ${transcription.text} \n ${config.WEB_URL}/search?q=${sub.regex}`);
+            await sendEmailWithRateLimit(sub.email, `${sub.regex}`, `${fileName}\n${allText}\n${config.WEB_URL}/search?q=${sub.regex}`);
         }
     }
 };
